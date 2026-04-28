50b0d3c3664a4ac65d384be0ccc397e7f43c1ed6
Cova <alxsanchezc07@gmail.com>
2026-04-27 22:01:11 -0600

    Updated backend configuration and added error handling in recommendator

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,4 +3,5 @@
 export const environment = {
   supabaseUrl: 'https://fcpljjxdzeqwdiidwxdn.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjcGxqanhkemVxd2RpaWR3eGRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3Njg4NDQsImV4cCI6MjA4NjM0NDg0NH0.LwacZ-zBnRS05TigjoAWcri1sLbPLwVBwAcEmP9ZVTU',
+  apiUrl: 'http://localhost:5105',
 };
